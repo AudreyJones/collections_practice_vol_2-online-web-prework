5a9a5b0e1a8f3331a872f56db9ab420199f01e2a
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-08 20:43:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
-# your code goes here
+require 'pry'
+def begins_with_r(array)
+  i = 0
+  while i < array.length
+    if array.all? { |i| i.start_with?("r")  }
+      return true
+    else
+      return false
+    end
+    i += 1
+  end
+end
+
+def contain_a(array)
+  i = 0
+  a_collection = []
+  while i < array.length
+    if array[i].include?("a")
+      a_collection << array[i]
+    end
+    i += 1
+  end
+  a_collection
+end
+
+def first_wa(array)
+  i = 0
+  strings_only = []
+  strings_only = array.map { |i| i.to_s }
+  while i < array.length
+    if strings_only[i].start_with?("wa")
+      return array[i]
+    end
+    i += 1
+  end
+end
+
+# def remove_non_strings(array)
+#   i = 0
+#   while i < array.length
+#     i += 1
+#   end
+# end
