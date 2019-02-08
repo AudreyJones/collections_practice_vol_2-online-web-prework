f33413e98d3482aa71bf73ce8f781bdf1b13a7e4
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-08 20:52:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,7 +38,7 @@ end
 def remove_non_strings(array)
   i = 0
   while i < array.length
-    if array[i].is_a?(symbol) == true
+    if array[i].is_a? Symbol == true
       #remove it from the array
         array.delete(array[i])
 binding.pry
