821e8598d41b6a6f18a422240f068a1a352c4b01
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-08 20:50:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,7 +40,8 @@ def remove_non_strings(array)
   while i < array.length
     if array[i].is_a?(sym) == true
       #remove it from the array
-      #else, do nothing!
+        array.delete(array[i])
+    #else, do nothing!
     end
     i += 1
   end
